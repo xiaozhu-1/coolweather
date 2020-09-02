e22a6168a19d8ecdbfef7d782bbc3efc1851674d
xiaozhu <1370897146@qq.com>
2020-09-02 15:32:53 +0800

    加入创建数据库和表的各项配置。‘

---
+++ app/src/main/java/cn/org/coolweather/db/City.java
@@ -0,0 +1,42 @@
+package cn.org.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;//市的id
+    private String cityName;//市的名字
+    private int cityCode;//市的代号
+    private int provinceId;//当前所属省份的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/cn/org/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package cn.org.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;//县的ID
+    private String countyName;//县的名字
+    private String weatherId;//县对应的天气id
+    private int cityId;//所属市的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
